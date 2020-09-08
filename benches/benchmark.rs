49bf8c6ff5b2bea9d82bffb3181e33ea0f536008
Julian Wiesler <wiesleju@gmail.com>
2020-09-09 00:35:59 +0200

    New compare routine

---
+++ benches/benchmark.rs
@@ -0,0 +1,44 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use natural_sort;
+use rand::prelude::*;
+use rand_chacha::ChaCha20Rng;
+use rand::distributions::Uniform;
+
+use natural_sort::{natural_cmp, natural_cmp_old};
+
+const CHARACTERS: &str = "ABCDEF123456";
+
+fn generate_string(rng: &mut ChaCha20Rng) -> String {
+    let count_distribution: Uniform<u32> = Uniform::new(1, 10);
+    let character_distribution: Uniform<u32> = Uniform::new(1, CHARACTERS.len() as u32);
+    let mut res = String::new();
+    let count = count_distribution.sample(rng);
+    res.reserve(count as usize);
+    for _ in 0..count {
+        res.push(char::from(CHARACTERS.as_bytes()[character_distribution.sample(rng) as usize]));
+    }
+    res
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("natural_cmp random", |b| b.iter(|| {
+        let mut rng = ChaCha20Rng::seed_from_u64(42);
+        for _ in 0..1000 {
+            let a = generate_string(&mut rng);
+            let b = generate_string(&mut rng);
+            black_box(natural_cmp(black_box(a.as_str()), black_box(b.as_str())));
+        }
+    }));
+
+    c.bench_function("natural_cmp_old random", |b| b.iter(|| {
+        let mut rng = ChaCha20Rng::seed_from_u64(42);
+        for _ in 0..1000 {
+            let a = generate_string(&mut rng);
+            let b = generate_string(&mut rng);
+            black_box(natural_cmp_old(black_box(a.as_str()), black_box(b.as_str())));
+        }
+    }));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
-use std::cmp::Ordering;
+use std::cmp::{min, Ordering};
+use std::cmp::Ordering::{Equal, Greater, Less};
 
 #[derive(PartialEq, Eq, Debug)]
-pub struct Chunk<'a> {
+struct Chunk<'a> {
     chars: &'a str,
     are_digits: bool,
 }
@@ -12,7 +13,7 @@ impl<'a> Chunk<'a> {
     }
 }
 
-pub fn natural_chunk(a: &str) -> Chunk {
+fn natural_chunk(a: &str) -> Chunk {
     let is_digit = match a.as_bytes().first() {
         None => return Chunk::new(a, false),
         Some(c) => c.is_ascii_digit(),
@@ -25,7 +26,7 @@ pub fn natural_chunk(a: &str) -> Chunk {
     Chunk::new(a, is_digit)
 }
 
-pub fn natural_cmp(mut a: &str, mut b: &str) -> Ordering {
+pub fn natural_cmp_old(mut a: &str, mut b: &str) -> Ordering {
     while !a.is_empty() && !b.is_empty() {
         let chunk_a = natural_chunk(a);
         let chunk_b = natural_chunk(b);
@@ -49,38 +50,162 @@ pub fn natural_cmp(mut a: &str, mut b: &str) -> Ordering {
     return a.len().cmp(&b.len());
 }
 
+pub fn natural_cmp(left: &str, right: &str) -> Ordering {
+    let l = min(left.len(), right.len());
+
+    // Slice to the loop iteration range to enable bound check
+    // elimination in the compiler
+    let lhs = &left.as_bytes()[..l];
+    let rhs = &right.as_bytes()[..l];
+
+    let mut i = 0;
+    let mut lc;
+    let mut rc;
+
+    // skip the equal characters
+    let res = loop {
+        if i == l {
+            // in this case the longer sequence wins (even if both ended in a number)
+            return left.len().cmp(&right.len());
+        }
+        lc = lhs[i];
+        rc = rhs[i];
+        let res = lc.cmp(&rc);
+        if !matches!(res, Equal) {
+            break res;
+        }
+        i = i + 1;
+    };
+
+    // special case: last char could have been a number
+    let l_digit = lc.is_ascii_digit();
+    let r_digit = rc.is_ascii_digit();
+    if !l_digit || !r_digit {
+        if 0 < i {
+            if r_digit {
+                if rhs[i - 1].is_ascii_digit() {
+                    return Less;
+                }
+            } else if l_digit {
+                if lhs[i - 1].is_ascii_digit() {
+                    return Greater;
+                }
+            }
+        }
+        return res;
+    }
+
+    // if the numbers turn out to have the same length, res is the result != Equal
+    // current situation:
+    // ...1
+    // ...2
+    //     ^
+    loop {
+        i = i + 1;
+        if i == l {
+            // in this case we don't know yet who wins
+            break;
+        }
+
+        let lc = lhs[i];
+        let rc = rhs[i];
+
+        let l_digit = lc.is_ascii_digit();
+        let r_digit = rc.is_ascii_digit();
+
+        if !l_digit && !r_digit {
+            // no digit => same length
+            return res;
+        } else if !l_digit {
+            return Less;
+        } else if !r_digit {
+            return Greater;
+        }
+    }
+
+    // current situation: (one sequence ended)
+    // ...1...?
+    // ...2...?
+    // find the longer number (that sequence wins)
+    // or number_ordering_same_length since we already found a distinguishing character
+    if left.len() != l {
+        if left.as_bytes()[l].is_ascii_digit() {
+            return Greater;
+        }
+    } else if right.len() != l {
+        if right.as_bytes()[l].is_ascii_digit() {
+            return Less;
+        }
+    }
+    res
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn natural_chunk_works() {
-        assert_eq!(Chunk::new("", false), natural_chunk(""));
-        assert_eq!(Chunk::new("A", false), natural_chunk("A"));
-        assert_eq!(Chunk::new("1", true), natural_chunk("1"));
-        assert_eq!(Chunk::new("123", true), natural_chunk("123"));
-        assert_eq!(Chunk::new("test", false), natural_chunk("test"));
-        assert_eq!(Chunk::new("1", true), natural_chunk("1Test"));
-        assert_eq!(Chunk::new("123", true), natural_chunk("123Test"));
-        assert_eq!(Chunk::new("Test", false), natural_chunk("Test1"));
-        assert_eq!(Chunk::new("Test", false), natural_chunk("Test123"));
-    }
-
-    #[test]
     fn natural_cmp_works() {
         assert_eq!(Ordering::Equal, natural_cmp("", ""));
-
         assert_eq!(Ordering::Equal, natural_cmp("1", "1"));
         assert_eq!(Ordering::Equal, natural_cmp("a", "a"));
+
+        assert_eq!(Ordering::Less, natural_cmp("ab", "ac"));
+        assert_eq!(Ordering::Greater, natural_cmp("ac", "ab"));
+
+        assert_eq!(Ordering::Less, natural_cmp("test1", "test12"));
+        assert_eq!(Ordering::Greater, natural_cmp("test12", "test1"));
+
         assert_eq!(Ordering::Less, natural_cmp("1", "2"));
         assert_eq!(Ordering::Greater, natural_cmp("2", "1"));
 
-        assert_eq!(Ordering::Less, natural_cmp("1", "12"));
-        assert_eq!(Ordering::Less, natural_cmp("2", "12"));
-        assert_eq!(Ordering::Greater, natural_cmp("24", "12"));
+        assert_eq!(Ordering::Less, natural_cmp("12", "13"));
+        assert_eq!(Ordering::Greater, natural_cmp("13", "12"));
 
-        assert_eq!(Ordering::Less, natural_cmp("test1", "test12"));
-        assert_eq!(Ordering::Less, natural_cmp("test2", "test12"));
-        assert_eq!(Ordering::Greater, natural_cmp("test24", "test12"));
+        assert_eq!(Ordering::Less, natural_cmp("1a", "12"));
+        assert_eq!(Ordering::Greater, natural_cmp("12", "1a"));
+
+        assert_eq!(Ordering::Greater, natural_cmp("aa", "a2"));
+        assert_eq!(Ordering::Less, natural_cmp("a2", "aa"));
+
+        assert_eq!(Ordering::Greater, natural_cmp("a", "1"));
+        assert_eq!(Ordering::Less, natural_cmp("1", "a"));
+
+        assert_eq!(Ordering::Less, natural_cmp("2", "12"));
+        assert_eq!(Ordering::Greater, natural_cmp("12", "2"));
+
+        assert_eq!(Ordering::Less, natural_cmp("12a", "22b"));
+        assert_eq!(Ordering::Greater, natural_cmp("22b", "12a"));
+
+        assert_eq!(Ordering::Less, natural_cmp("12a", "221"));
+        assert_eq!(Ordering::Greater, natural_cmp("221", "12a"));
+    }
+
+    use rand::prelude::*;
+    use rand_chacha::ChaCha20Rng;
+    use rand::distributions::Uniform;
+
+    const CHARACTERS: &str = "ABCDEF123456";
+
+    fn generate_string(rng: &mut ChaCha20Rng) -> String {
+        let count_distribution: Uniform<u32> = Uniform::new(1, 10);
+        let character_distribution: Uniform<u32> = Uniform::new(1, CHARACTERS.len() as u32);
+        let mut res = String::new();
+        let count = count_distribution.sample(rng);
+        res.reserve(count as usize);
+        for _ in 0..count {
+           res.push(char::from(CHARACTERS.as_bytes()[character_distribution.sample(rng) as usize]));
+        }
+        res
+    }
+
+    #[test]
+    fn natural_cmp_same_as_old_fuzz() {
+        let mut rng = ChaCha20Rng::seed_from_u64(42);
+        for _ in 0..10000 {
+            let a = generate_string(&mut rng);
+            let b = generate_string(&mut rng);
+            assert_eq!(natural_cmp_old(a.as_str(), b.as_str()), natural_cmp(a.as_str(), b.as_str()));
+        }
     }
 }
